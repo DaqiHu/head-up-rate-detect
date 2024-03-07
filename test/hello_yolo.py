94d916b4354867ca60c22088794176809b0cd602
Dutchi-Who <96360144+Dutchi-Who@users.noreply.github.com>
2024-03-07 13:50:52 +0800

    test

--- test/hello_yolo.py
+++ test/hello_yolo.py
@@ -1,4 +1,21 @@
 from ultralytics import YOLO, settings
+import json
 
-print(settings)
+json_settings = json.dumps(settings, indent=4)
 
+print(json_settings)
+
+
+model = YOLO("yolov8n.pt")
+results = model(source="bus.jpg")
+
+print(type(results[0]))
+
+for result in results:
+    boxes = result.boxes
+    masks = result.masks
+    keypoints = result.keypoints
+    probs = result.probs
+    print(boxes, masks, keypoints, probs)
+    result.show()
+    result.save(filename="runs/result.jpg")
